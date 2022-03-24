5b09f5fd94460f62f3ccc42bc24b162f473fb2e4
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-03-24 22:50:28 +0300

    Initial commit

---
+++ Assets/Source/Controllers/Initializer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initializer : ObjectModel
+{
+    [SerializeField] List<ObjectModel> objects;
+    public bool InitializeOnAwake;
+    bool isInitialized;
+
+    private void Awake()
+    {
+        if (InitializeOnAwake)
+        {
+            Initialize();
+        }
+    }
+
+
+    public override void Initialize()
+    {
+        if (isInitialized == false)
+        {
+            isInitialized = true;
+            for (int i = 0; i < objects.Count; i++)
+            {
+                objects[i].Initialize();
+            }
+
+            base.Initialize();
+        }
+    }
+
+}
