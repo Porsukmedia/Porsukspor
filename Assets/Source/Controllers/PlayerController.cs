daf643b0c6c144d0e1758dee2003574800c41af3
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-03-24 23:11:41 +0300

    player controls

--- Assets/Source/Controllers/PlayerController.cs
+++ Assets/Source/Controllers/PlayerController.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : ControllerBase
 {
-    // Start is called before the first frame update
-    void Start()
+    float walkSpeed = 5f;
+    Vector2 movement;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] PlayerModel playerModel;
+
+    void Movement()
     {
-        
+        movement.x = Input.GetAxis("Horizontal") * walkSpeed * Time.deltaTime;
+        movement.y = Input.GetAxis("Vertical") * walkSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+        //animation
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Interact()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            playerModel.OnInteract();
+            //interaction
+        }
     }
+
+    public override void ControllerUpdate()
+    {
+        base.ControllerUpdate();
+        Movement();
+    }
+
+    private void Update()
+    {
+        if (GameStateController.CurrentState == GameState.Game)
+        {
+            ControllerUpdate();
+        }
+    }
+
 }
--- Assets/Source/Models/PlayerModel.cs
+++ Assets/Source/Models/PlayerModel.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class PlayerModel : ObjectModel
 {
-    
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public void OnLevelStarted()
+    {
+
+    }
+
+    public void OnInteract()
+    {
+
+    }
+
+    public void OnLevelFinished()
+    {
+
+    }
+
 
 }
